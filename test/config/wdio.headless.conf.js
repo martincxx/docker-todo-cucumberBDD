488fcc06fc9ec3d24236d7ef837c0c9ba6609f7d
Martin Coronel <martincxx@gmail.com>
2020-10-14 22:37:38 +0300

    New commands

--- test/config/wdio.headless.conf.js
+++ test/config/wdio.headless.conf.js
@@ -6,15 +6,15 @@ exports.config = {
       services: [],
       capabilities: [
           {
-            maxInstances: 5,
+            maxInstances: 1,
             browserName: 'chrome',
             'goog:chromeOptions': {
               // For more information about headless Chrome see https://developers.google.com/web/updates/2017/04/headless-chrome
-              args: ['--headless', '--disable-gpu'],
+              args: ['--headless', '--disable-gpu', '--no-sandbox'],
             }
         },
         {
-          maxInstances: 5,
+          maxInstances: 1,
           browserName: 'firefox',
           "moz:firefoxOptions": {
             // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
